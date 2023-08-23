13387fd53ea31d322f7643fc3570324f580f04be
tengyu <tengyu@tencent.com>
2023-08-23 16:41:20 +0800

    优化 streaming

--- hooks/useLLM.js
+++ hooks/useLLM.js
@@ -28,17 +28,48 @@ const resolveStream = async ({
       const stream = await reader.read();
       if (stream.done) break;
   
-      const chunks = stream?.value
-        .replaceAll(/^data: /gm, "")
-        .split("\n")
-        .filter((c) => Boolean(c.length) && c !== "[DONE]")
-        .map((c) => JSON.parse(c));
+      // const chunks = stream?.value
+      //   .replaceAll(/^data: /gm, "")
+      //   .split("\n")
+      //   .filter((c) => Boolean(c.length) && c !== "[DONE]")
+      //   .map((c) => JSON.parse(c));
+
+
+      let buffer = "";
   
-      for (let chunk of chunks) {
-        const content = chunk.choices[0].delta.content;
-        if (!content) continue;
-        data.value += chunk.choices[0].delta.content;
-        onChunk({ data: content });
+      for (let chunk of stream?.value) {
+        buffer += chunk;
+        const lines = buffer.split("\n");
+
+        if(buffer.endsWith("\n")) {
+          buffer = "";
+        } else {
+          buffer = lines.pop()|| "";
+        }
+
+        for (let line of lines) {
+          const message = line.trim().split("data: ")[1];
+          if (message === "[DONE]") {
+            break;
+          }
+
+          if(message){
+            try {
+              const json = JSON.parse(message);
+              const content = json.choices[0].delta.content;
+              if (!content) continue;
+              data.value += content;
+              onChunk({ data: content });
+            }catch(e){
+              console.error("Error parsing JSON message:", error);
+            }
+          }
+        }
+
+        // const content = chunk.choices[0].delta.content;
+        // if (!content) continue;
+        // data.value += chunk.choices[0].delta.content;
+        // onChunk({ data: content });
       }
     }
   
