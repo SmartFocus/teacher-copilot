653783680ab7786ab14e7102ef65092770780f2c
tengyu <tengyu@tencent.com>
2023-08-23 14:21:34 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "path";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@element-plus/nuxt'
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@element-plus/nuxt"],
   elementPlus: {
-    themes: ['dark']
+    themes: ["dark"],
+  },
+  // You might not need it if you're not using pnpm
+  alias: {
+    "node:util": path.resolve(
+      __dirname,
+      "node_modules/unenv/runtime/node/util/index.cjs"
+    ),
+    "node:net": path.resolve(
+      __dirname,
+      "node_modules/unenv/runtime/node/net/index.cjs"
+    ),
   },
   runtimeConfig: {
     openai: {
       basePath: process.env.OPENAI_BASE_PATH,
-      apiKey: process.env.OPENAI_API_KEY
-    }
-  }
-})
+      apiKey: process.env.OPENAI_API_KEY,
+    },
+  },
+});
